dc8575f596937d93aa30c80d0c9ab55bde5f85b2
TalismanTechnology <lucashaines2010@gmail.com>
2026-07-12 20:35:51 -0400

    Make footer background transparent

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="bg-black border-t border-white/10">
+    <footer className="bg-transparent border-t border-white/15">
       <div className="max-w-[1340px] mx-auto px-[15px] py-9 md-tablet:py-[30px] md-tablet:px-[18px] mobile:py-6 mobile:px-[18px]">
         <div className="flex items-center justify-between gap-6">
           <p className="text-xs leading-3 tracking-[-0.08px] font-medium uppercase text-white/40">
