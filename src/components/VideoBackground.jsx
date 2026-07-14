ddfbd5bdeebcc99c9346231b44c0be2983a1ff35
TalismanTechnology <lucashaines2010@gmail.com>
2026-07-13 20:22:42 -0400

    Add painterly surreal hero videos

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,9 +1,9 @@
 import { useRef } from 'react'
 
 export const VIDEO_URLS = [
-  'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260629_030107_874273ea-684a-4e90-bb96-8fdfde48d53d.mp4',
-  'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260629_032424_3c9c2a9d-807b-4482-80e6-dd6d9dfd4545.mp4',
-  'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260627_094019_4214ea73-b963-46a4-8327-61489192de99.mp4',
+  '/assets/hero/painterly-01.mp4',
+  '/assets/hero/painterly-02.mp4',
+  '/assets/hero/painterly-03.mp4',
 ]
 
 export default function VideoBackground({ activeIndex = 0 }) {
